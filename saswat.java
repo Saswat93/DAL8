8b923f8ea8e5694bb8d648075ee8ef72c1cac30d
Saswat Pattnaik <saswatpattnaik21@gmail.com>
2021-02-24 10:31:02 +0530

    Hello

--- saswat.java
+++ saswat.java
@@ -2,11 +2,22 @@ public class saswat
 {
 	public static void main (String args[])
 	{
+<<<<<<< HEAD
 		System.out.println ("Hello Saswat");
 
 		System.out.print("Devops is uber cool");
 
 		System.out.println (" Devops World");
+=======
+
+		System.out.println ("Hello Saswat is  DEvops Architect");
+		System.out.println ("Hello Mihir is a Devops Champ");
+
+		System.out.println ("Hello Saswat//");
+
+
+
+>>>>>>> b788c19e383622ebeddcc9aa698be60b5c84cb31
 
 	}
 }
